6c3e701208e15271ae8dfc51cdce06f3bd5ec73b
BaraaZ95 <80360467+BaraaZ95@users.noreply.github.com>
2023-02-03 10:50:02 +0200

    added release_date

--- game_benchmark/spiders/games.py
+++ game_benchmark/spiders/games.py
@@ -4,7 +4,7 @@ import scrapy
 class GamesSpider(scrapy.Spider):
     name = 'games'
     #allowed_domains = ['www.pcgamebenchmark.com']
-    start_urls = [f'https://www.pcgamebenchmark.com/best-pc-games/page-{i}?tags=&sort=0' for i in range(1, 6933)] 
+    start_urls = [f'https://www.pcgamebenchmark.com/best-pc-games/page-{i}?tags=&sort=0' for i in range(1, 6933)]
 
     def parse(self, response):
         urls =  response.css('div[class="games-list"]>div>div>a::attr(href)').getall()
@@ -13,12 +13,16 @@ class GamesSpider(scrapy.Spider):
             yield scrapy.Request(url, callback= self.parse_page)
             
     def parse_page(self, response):
+        dict_ = dict()
+        dict_['name'] = response.css('h1::text').get()  
         
         
         specs = response.css('div[class="six columns"]> ul[class="bb_ul"]> li>strong::text').getall()
         text =  response.css('div[class="six columns"]> ul[class="bb_ul"]> li::text').getall()
+        table = response.css('table > tr ::text').getall() #For getting the release date
+        table = [i for i in table if i.strip()]
         dict_ = dict(zip(specs, text))
-        dict_['name'] = response.css('h1::text').get()
+        dict_['Release_date'] = table[-1].strip()
         yield dict_
         
         
